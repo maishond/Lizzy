d568be8671f5f0bb4ed8f8a23ef0dcf311705454
Jip Frijlink <jipfrijlink@gmail.com>
2026-05-24 21:02:19 +0000

    lol

---
+++ computercraft-client/aero5/point2.lua
@@ -0,0 +1,15 @@
+-- Write baseUrl to file
+local file = fs.open('startup-override.lua', 'w')
+file.write("require('update')\nrequire('point2')")
+file.close()
+
+print('hi2')
+
+modem = peripheral.find('modem')
+
+while true do
+	x, y, z = gps.locate(0.1)
+	print(x, y, z)
+	modem.transmit(42, 2, string.format('%s %s %s', x, y, z))
+	sleep(0.05)
+end
---
+++ computercraft-client/aero5/utils.lua
@@ -0,0 +1,89 @@
+function split(str, delimiter)
+    local result = {}
+
+    for v in string.gmatch(str, "([^" .. delimiter .. "]+)") do
+        result[#result + 1] = v
+    end
+
+    return result
+end
+
+function clamp(min, v, max)
+    if v > max then return max end
+    if v < min then return min end
+    return v
+end
+
+function angle_diff(a, b)
+	local d = a - b
+
+	while d > math.pi do
+		d = d - math.pi * 2
+	end
+
+	while d < -math.pi do
+		d = d + math.pi * 2
+	end
+
+	return d
+end
+
+function get_state()
+	-- ! Get P2 pos (rear)
+    local event, side, channel, replyChannel, message, distance
+
+    repeat
+        event, side, channel, replyChannel, message, distance =
+            os.pullEvent("modem_message")
+    until channel == 42
+
+    local spl = split(message, " ")
+
+    local p2_x = tonumber(spl[1])
+    local p2_y = tonumber(spl[2])
+    local p2_z = tonumber(spl[3])
+
+	-- ! P3 pos (right)
+    repeat
+        event, side, channel, replyChannel, message, distance =
+            os.pullEvent("modem_message")
+    until channel == 41
+
+    spl = split(message, " ")
+
+    local p3_x = tonumber(spl[1])
+    local p3_y = tonumber(spl[2])
+    local p3_z = tonumber(spl[3])
+
+	-- ! Self
+    local x, y, z = gps.locate(0.1)
+    if x and p2_x and p3_x then
+
+		local p2_x_diff = p2_x - x
+		local p2_y_diff = p2_y - y
+		local p2_z_diff = p2_z - z
+
+		local p3_dx = p3_x - x
+		local p3_dy = p3_y - y
+		local p3_dz = p3_z - z
+
+
+		-- ! Pitch
+		local horizontal_pitch = math.sqrt(p2_x_diff * p2_x_diff + p2_z_diff * p2_z_diff)
+		local pitch = math.atan2(p2_y_diff, horizontal_pitch) * 180 / math.pi
+
+
+		-- ! Yaw
+		local yaw = 360 - (180 + math.atan2(p2_x_diff, p2_z_diff) * 180 / math.pi)
+		while yaw < 0 do
+			yaw = yaw + 360
+		end
+
+
+		-- ! Roll
+		local horizontal_roll =math.sqrt(p3_dx * p3_dx + p3_dz * p3_dz)
+		local roll = math.atan2(p3_dy, horizontal_roll) * 180 / math.pi
+
+		return x, y, z, pitch, yaw, roll
+	end
+end
