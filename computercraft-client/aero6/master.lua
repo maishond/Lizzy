dad316b23366c6a639fe2c582a2832f83c1b71d6
Jip Frijlink <jipfrijlink@gmail.com>
2026-06-06 17:33:34 +0000

    a6

---
+++ computercraft-client/aero6/master.lua
@@ -0,0 +1,53 @@
+-- Write startup override
+local file = fs.open("startup-override.lua", "w")
+file.write("require('update')\nrequire('master')")
+file.close()
+
+
+-- ! Ports docs
+-- ! 42 is point 2 XYZ (back)
+-- ! 41 is point 3 XYZ (side)
+-- ! 43 is yaw_error (emitted by master computer)
+------------
+
+require 'mods'
+
+redstone.setAnalogOutput('left', 15)
+redstone.setAnalogOutput('right', 15)
+redstone.setAnalogOutput('front', 15)
+
+print("Flight controller starting...")
+
+local modem = peripheral.wrap("back")
+
+modem.open(42) -- p2 (back)
+modem.open(41) -- p3 (side)
+
+
+-- ! Main loop
+print('Main initialised')
+
+-- thou = 1000
+-- stabilise_at(50 * thou, 0, 0, 0, 0, 0)
+
+-- stabilise_at(-283, 95, -177, 0, 0, 0) -- wouter airstrip
+-- stabilise_at(-348, 136, 2, 0, 0, 0) -- wouter huis
+-- stabilise_at(-1686, 78, -186, 0, 0, 0) -- casper
+-- stabilise_at(-608, 42, 2034, 0, 0, 0) -- ocean
+-- stabilise_at(-1258, 69, -2934, 0, 0, 0) -- NEW airport
+
+-- stabilise_at(0, 0, 0, 0, 0, 0)
+-- stabilise_at(299, 0, 2286, 0, 0, 0)
+-- stabilise_at(2500, 0, 2500, 0, 0, 0)
+-- stabilise_at(-1000, 0, 0, 0, 0, 0)
+-- stabilise_at(5000, 0, 0, 0, 0, 0)
+
+b = 30000
+-- stabilise_at(0, 0)
+-- stabilise_at(b, 0)
+-- stabilise_at(b, b)
+-- stabilise_at(-b, b)
+-- stabilise_at(-b, -b)
+-- stabilise_at(b, -b)
+-- stabilise_at(b, 0)
+-- stabilise_at(-348, 2) -- wouter huis
---
+++ computercraft-client/aero6/utils.lua
@@ -0,0 +1,96 @@
+function split(str, delimiter)
+    local result = {}
+
+    for v in string.gmatch(str, "([^" .. delimiter .. "]+)") do
+        result[#result + 1] = v
+    end
+
+    return result
+end
+
+function clamp(min, v, max)
+    if v > max then return max end
+    if v < min then return min end
+    return v
+end
+
+function angle_diff(a, b)
+	local d = a - b
+
+	while d > math.pi do
+		d = d - math.pi * 2
+	end
+
+	while d < -math.pi do
+		d = d + math.pi * 2
+	end
+
+	return d
+end
+
+function get_state()
+	-- ! Get P2 pos (rear)
+    local event, side, channel, replyChannel, message, distance
+
+	modem = peripheral.wrap('back')
+	modem.open(42)
+	modem.open(41)
+
+    repeat
+        event, side, channel, replyChannel, message, distance =
+            os.pullEvent("modem_message")
+    until channel == 42
+
+    local spl = split(message, " ")
+
+    local p2_x = tonumber(spl[1])
+    local p2_y = tonumber(spl[2])
+    local p2_z = tonumber(spl[3])
+
+	-- ! P3 pos (right)
+    repeat
+        event, side, channel, replyChannel, message, distance =
+            os.pullEvent("modem_message")
+    until channel == 41
+
+    spl = split(message, " ")
+
+    local p3_x = tonumber(spl[1])
+    local p3_y = tonumber(spl[2])
+    local p3_z = tonumber(spl[3])
+
+	-- ! Self
+    local x, y, z = gps.locate(0.1)
+    if x and p2_x and p3_x then
+
+		local p2_x_diff = p2_x - x
+		local p2_y_diff = p2_y - y
+		local p2_z_diff = p2_z - z
+
+		local p3_x_diff = p3_x - x
+		local p3_y_diff = p3_y - y
+		local p3_z_diff = p3_z - z
+
+		-- ! Pitch
+		local horizontal_pitch = math.sqrt(p2_x_diff * p2_x_diff + p2_z_diff * p2_z_diff)
+		local pitch = math.atan2(p2_y_diff, horizontal_pitch) * 180 / math.pi
+
+
+		-- ! Yaw
+		-- print(math.floor(x), math.floor(y), math.floor(z))
+		-- print(math.floor(p2_x), math.floor(p2_y), math.floor(p2_z))
+		-- print(math.floor(p3_x), math.floor(p3_y), math.floor(p3_z))
+		-- print(math.floor(p2_x_diff), math.floor(p2_z_diff))
+		local yaw = 360 - (180 + math.atan2(p2_x_diff, p2_z_diff) * 180 / math.pi)
+		while yaw < 0 do
+			yaw = yaw + 360
+		end
+
+
+		-- ! Roll
+		local horizontal_roll =math.sqrt(p3_x * p3_x + p3_z_diff * p3_z_diff)
+		local roll = math.atan2(p3_y_diff, horizontal_roll) * 180 / math.pi
+
+		return x, y, z, pitch, yaw, roll
+	end
+end
